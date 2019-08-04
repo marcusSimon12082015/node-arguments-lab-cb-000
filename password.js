a223361bce6af4af2018c2e8dec507cbd6f684b2
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-04 17:29:42 +0000

    Automatically backed up by Learn

--- password.js
+++ password.js
@@ -7,10 +7,11 @@ var numberOfPasswords = process.argv.shift()
 var arrayOfPasswords = new Array()
 
 for(var i = 0; i < numberOfPasswords; i++){
-
+  
 }
 
 arrayOfPasswords.forEach(function(passsword){
   console.log(element)
 });
+
 process.exit(0)
