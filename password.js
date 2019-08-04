38423f94916134d8c1590cba0eab012fae91e5dc
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-04 17:54:42 +0000

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,15 @@
 // code solution here
+process.argv.shift()
+process.argv.shift()
+var length = process.argv.shift()
+var numberOfPasswords = process.argv.shift()
+
+for(var i = 0; i < numberOfPasswords; i++){
+  var newPassword = ""
+  for(var j = 0; j < length; j++){
+    newPassword += Math.floor(Math.random() * 9);
+  }
+  console.log(newPassword)
+}
+
+process.exit(0)
